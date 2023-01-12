53c8c616a1a30fb01bdb9ab9906db16bb694da9a
Mindespa <mindaugas.strong@gmail.com>
2023-01-12 18:34:08 +0200

    main.js 3 uzduotis

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+console.log(" 1 uzduotis");
+
+let name = "Benicio";
+let surname = "Deltoro";
+console.log(name.length);
+console.log(surname.length);
+if (name.length < surname.length) {
+    console.log(name);
+} else if (name.length = surname.length) {
+    console.log(name + " " + surname);
+} else {
+    console.log(surname);
+}
+
+console.log(" 2 uzduotis");
+
+
+let name2 = "Benicio";
+let name3 = "Deltoro";
+console.log(name2.toUpperCase() + " " + name3.toLowerCase());
+
+console.log(" 3 uzduotis");
+
+let actname = "Benicio";
+let actsrnm = "Deltoro";
+let nmsrnm = "";
+nmsrnm += actname.charAt(0);
+nmsrnm = nmsrnm + actname.charAt(0);
+nmsrnm += actsrnm.charAt(0);
+nmsrnm = nmsrnm + actsrnm.charAt(0);
+console.log(actname.charAt(0)+actsrnm.charAt(0));
+console.log(nmsrnm);
