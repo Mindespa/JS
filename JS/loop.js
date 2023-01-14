d1f2b74389d5bc940081efc043ae8bbf4c01cf7e
Mindespa <mindaugas.strong@gmail.com>
2023-01-14 17:23:46 +0200

    js.loop.6 uzduotis

---
+++ JS/loop.js
@@ -0,0 +1,21 @@
+console.log("  1 uzduotis");
+
+for (let i = 0; i < 10; i++) {
+    console.log("labas");
+    
+}
+
+console.log("  2 uzduotis");
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+console.log("  6 uzduotis");
+
+for (let i = 10; i < 50; i++) {
+    if (i%2==0) {
+        console.log(i);
+    }
+    
+}
