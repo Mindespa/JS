3a06b7301aa866401c647821da1cc7b470e07562
Mindespa <mindaugas.strong@gmail.com>
2023-01-16 21:21:29 +0200

    js.loops 3.4.5.7 uzduotys

--- JS/loop.js
+++ JS/loop.js
@@ -11,6 +11,29 @@ for (let i = 0; i < 10; i++) {
     console.log(i);
 }
 
+console.log("  3 uzduotis");
+
+let sk = 0;
+
+let augalai = ["zole","roze","braske","tulpe","obelis","berzas","klevas","citrina","rugiai","salieras"];
+console.log(augalai);
+
+console.log("  4 uzduotis");
+
+let augalai1 = ["zole","roze","braske","tulpe","obelis","berzas","klevas","citrina","rugiai","salieras"];
+for (let i = 0; i < augalai1.length; i++){
+    console.log(augalai1[i]);
+}
+
+console.log("  5 uzduotis");
+ 
+let augalai2 = ["zole","roze","braske","tulpe","obelis","berzas","klevas","citrina","rugiai","salieras"];
+for (let i = augalai2.length -1; i >= 0; i--){
+    console.log(augalai2[i]);
+
+}
+
+
 console.log("  6 uzduotis");
 
 for (let i = 10; i < 50; i++) {
@@ -23,9 +46,13 @@ for (let i = 10; i < 50; i++) {
 console.log("  7 uzduotis");
 
 for (let i = 10; i < 50; i++) {
-    if (i%2==0 || i%10==0) {
+    if (i%2==0) {
+       if (i%10==0) {
         continue;
+        
+       }
+        console.log(i);
     }    
-    console.log(i);
     
 }
+
